f34535ab88d4cbbd43f4beb710d8ba7cd14ecfbe
David Avila <davidavila6002@gmail.com>
2025-07-05 16:11:50 -0600

    toggle effects button working

--- js/handle_permissions_effects.js
+++ js/handle_permissions_effects.js
@@ -25,11 +25,13 @@ function deactivateEffects() {
    const thunder_sound = document.getElementById("thunder-sound");
    const cursor_light = document.getElementById("cursor-light");
    const overlay_2 = document.getElementsByClassName("overlay-2");
+   const effects_toggle = document.getElementById("effects-toggle");
 
    rain_sound.muted = true;
    thunder_sound.muted = true;
    cursor_light.style.display = "none";
    overlay_2[0].style.display = "none";
+   effects_toggle.classList.remove("active");
 }
 
 function activateEffects() {
@@ -37,9 +39,11 @@ function activateEffects() {
    const thunder_sound = document.getElementById("thunder-sound");
    const cursor_light = document.getElementById("cursor-light");
    const overlay_2 = document.getElementsByClassName("overlay-2");
+   const effects_toggle = document.getElementById("effects-toggle");
 
    rain_sound.muted = false;
    thunder_sound.muted = false;
    cursor_light.style.display = "flex";
    overlay_2[0].style.display = "flex";
+   effects_toggle.classList.add("active");
 }
