767d8dba5727bda21a41c9cdff081a72b105500e
David Avila <davidavila6002@gmail.com>
2025-08-01 15:28:40 -0600

    cursor light effect Y scroll bug fixed

--- js/cursor_light.js
+++ js/cursor_light.js
@@ -1,10 +1,29 @@
 const body = document.body;
 const cursorLight = document.getElementById("cursor-light");
 
-body.addEventListener("mousemove", (e) => {
-   body.style.setProperty("--mouse-x", `${e.clientX}px`);
-   body.style.setProperty("--mouse-y", `${e.clientY}px`);
+let lastClientX = 0;
+let lastClientY = 0;
 
-   cursorLight.style.left = `${e.clientX}px`;
-   cursorLight.style.top = `${e.clientY}px`;
+function updateLightPosition(clientX, clientY) {
+   const x = clientX + window.scrollX;
+   const y = clientY + window.scrollY;
+
+   body.style.setProperty("--mouse-x", `${x}px`);
+   body.style.setProperty("--mouse-y", `${y}px`);
+   cursorLight.style.left = `${x}px`;
+   cursorLight.style.top = `${y}px`;
+}
+
+body.addEventListener("mousemove", (e) => {
+   lastClientX = e.clientX;
+   lastClientY = e.clientY;
+   updateLightPosition(lastClientX, lastClientY);
 });
+
+window.addEventListener(
+   "scroll",
+   () => {
+      updateLightPosition(lastClientX, lastClientY);
+   },
+   { passive: true }
+);
