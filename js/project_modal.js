72fdbf78dfb05b04ce4c911e0baef51d446c6fa1
David Avila <davidavila6002@gmail.com>
2025-08-23 16:25:18 -0600

    dynamic projectConstant

--- js/project_modal.js
+++ js/project_modal.js
@@ -1,12 +1,19 @@
-document
-   .querySelector(".card-project-viewproject-btn")
-   .addEventListener("click", () => {
+document.querySelectorAll(".card-project-viewproject-btn").forEach((btn) =>
+   btn.addEventListener("click", () => {
+      const projectCardBack = btn.closest(".project-card-back");
+      const projectName = projectCardBack
+         .querySelector(".card-project-title")
+         ?.textContent.trim();
+      cleanProjectInfo();
+      setProjectsInfo(projectName);
+
       document.querySelector("main").style["zIndex"] = 0;
       document.body.style.overflowY = "hidden";
       const overlay = document.querySelector(".project-modal-overlay");
       overlay.style.opacity = 1;
       overlay.style["pointerEvents"] = "auto";
-   });
+   })
+);
 
 document
    .querySelector(".project-modal-close-btn")
@@ -17,3 +24,45 @@ document
       overlay.style.opacity = 0;
       overlay.style["pointerEvents"] = "none";
    });
+
+const setProjectsInfo = (projectName) => {
+   const projectTitle = document.querySelector(".project-modal-content-title");
+   const projectStoryContainer = document.querySelector(
+      ".project-modal-content-story"
+   );
+   projectName = projectName.toLowerCase();
+
+   const currentProjectInfo = projectConstants[projectName];
+
+   if (!currentProjectInfo) {
+      projectTitle.textContent = "Unknown";
+
+      return;
+   }
+
+   projectTitle.textContent = currentProjectInfo.title;
+   currentProjectInfo.story.forEach((paragraph) => {
+      const p = document.createElement("p");
+      p.textContent = paragraph;
+      p.classList.add("project-modal-story-text");
+      p.classList.add("reveal");
+      projectStoryContainer.append(p);
+   });
+};
+
+const cleanProjectInfo = () => {
+   const projectTitle = document.querySelector(".project-modal-content-title");
+   const projectStoryContainer = document.querySelector(
+      ".project-modal-content-story"
+   );
+
+   projectTitle.textContent = "Unknown";
+   projectStoryContainer.innerHTML = "";
+};
+
+const projectConstants = {
+   "villamar rewards": {
+      title: "Villamar Rewards",
+      story: ["En el hotel Villamar princesa"],
+   },
+};
