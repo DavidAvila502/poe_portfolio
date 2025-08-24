a2d7ea37f3ebe6300b051b45fe0faa4cb241f4a1
David Avila <davidavila6002@gmail.com>
2025-08-24 11:38:40 -0600

    text animationa dded

--- js/project_modal.js
+++ js/project_modal.js
@@ -44,9 +44,11 @@ const setProjectsInfo = (projectName) => {
    currentProjectInfo.story.forEach((paragraph) => {
       const p = document.createElement("p");
       p.textContent = paragraph;
-      p.classList.add("project-modal-story-text");
-      p.classList.add("reveal");
+      p.classList.add("project-modal-story-text", "reveal");
       projectStoryContainer.append(p);
+
+      splitTextByChar(p);
+      io.observe(p);
    });
 };
 
@@ -63,6 +65,9 @@ const cleanProjectInfo = () => {
 const projectConstants = {
    "villamar rewards": {
       title: "Villamar Rewards",
-      story: ["En el hotel Villamar princesa"],
+      story: [
+         "El Hotel *Villamar Princesa* vivía bajo una pequeña pero persistente calamidad: *las antiguas tarjetas de membresía*, objetos cotidianos que el personal llamaba en voz baja “las fichas perdidas”, parecían desvanecerse de los bolsillos de los huéspedes en los momentos más inoportunos. Lo que al principio fue un susurro de incomodidad pronto se convirtió en una deuda tangible: reimpresiones costosas, cajas registradoras abiertas sin resultados y la sensación, casi supersticiosa, de que algo —una falla, un descuido o una suerte de maldición administrativa— acechaba los pasillos.",
+         "La solución, concebida con rigor y discreción, fue desterrar lo físico y conjurar una *alternativa digital*: una tarjeta de membresía segura, robusta y fácil de usar que reemplazó la vulnerabilidad por la confianza. Así, el Hotel Villamar Princesa recuperó su economía y su calma; la vieja inquietud se disolvió como niebla en la mañana, y lo que quedaba era un sistema eficiente, transparente y digno de su leyenda.",
+      ],
    },
 };
