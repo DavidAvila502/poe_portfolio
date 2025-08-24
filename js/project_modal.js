a9da382c0456555d5c0b70dbd9c6e725ef410408
David Avila <davidavila6002@gmail.com>
2025-08-24 14:14:00 -0600

    screenshots added

--- js/project_modal.js
+++ js/project_modal.js
@@ -30,6 +30,13 @@ const setProjectsInfo = (projectName) => {
    const projectStoryContainer = document.querySelector(
       ".project-modal-content-story"
    );
+   const projectTechContainer = document.querySelector(
+      ".project-modal-content-tech-container"
+   );
+
+   const projectScreenShots = document.querySelector(
+      ".project-modal-content-screenShots"
+   );
    projectName = projectName.toLowerCase();
 
    const currentProjectInfo = projectConstants[projectName];
@@ -50,6 +57,25 @@ const setProjectsInfo = (projectName) => {
       splitTextByChar(p);
       io.observe(p);
    });
+
+   currentProjectInfo.technologies.forEach((tech) => {
+      const img = document.createElement("img");
+      img.src = tech.image;
+      img.alt = tech.name;
+      img.title = tech.name;
+      img.classList.add("project-modal-content-tech-icon");
+
+      projectTechContainer.append(img);
+   });
+
+   currentProjectInfo.screenshots.forEach((shot, index) => {
+      const img = document.createElement("img");
+      img.src = shot;
+      img.alt = `screenshot${index + 1}`;
+      img.title = `screenshot${index + 1}`;
+
+      projectScreenShots.append(img);
+   });
 };
 
 const cleanProjectInfo = () => {
@@ -58,8 +84,17 @@ const cleanProjectInfo = () => {
       ".project-modal-content-story"
    );
 
+   const projectTechContainer = document.querySelector(
+      ".project-modal-content-tech-container"
+   );
+   const projectScreenShots = document.querySelector(
+      ".project-modal-content-screenShots"
+   );
+
    projectTitle.textContent = "Unknown";
    projectStoryContainer.innerHTML = "";
+   projectTechContainer.innerHTML = "";
+   projectScreenShots.innerHTML = "";
 };
 
 const projectConstants = {
@@ -68,6 +103,20 @@ const projectConstants = {
       story: [
          "El Hotel *Villamar Princesa* vivía bajo una pequeña pero persistente calamidad: *las antiguas tarjetas de membresía*, objetos cotidianos que el personal llamaba en voz baja “las fichas perdidas”, parecían desvanecerse de los bolsillos de los huéspedes en los momentos más inoportunos. Lo que al principio fue un susurro de incomodidad pronto se convirtió en una deuda tangible: reimpresiones costosas, cajas registradoras abiertas sin resultados y la sensación, casi supersticiosa, de que algo —una falla, un descuido o una suerte de maldición administrativa— acechaba los pasillos.",
          "La solución, concebida con rigor y discreción, fue desterrar lo físico y conjurar una *alternativa digital*: una tarjeta de membresía segura, robusta y fácil de usar que reemplazó la vulnerabilidad por la confianza. Así, el Hotel Villamar Princesa recuperó su economía y su calma; la vieja inquietud se disolvió como niebla en la mañana, y lo que quedaba era un sistema eficiente, transparente y digno de su leyenda.",
+      ],
+
+      technologies: [
+         { name: "React js", image: "./assets/image/react_icon.svg" },
+         { name: "Postgresql", image: "./assets/image/postgre_sql_icon.svg" },
+         { name: "strapi", image: "./assets/image/strapi_icon.svg" },
+      ],
+
+      screenshots: [
+         "./assets/image/villamar_rewards_1.jpeg",
+         "./assets/image/villamar_rewards_2.jpeg",
+         "./assets/image/villamar_rewards_3.jpeg",
+         "./assets/image/villamar_rewards_4.jpeg",
+         "./assets/image/villamar_rewards_5.jpeg",
       ],
    },
 };
